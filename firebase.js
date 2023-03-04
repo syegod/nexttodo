c3945acd033a5e1267fe2f22da80fb87536042cc
syegod <ovisochin362@gmail.com>
2023-03-04 19:09:35 +0100

    little changes

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import {getFirestore} from 'firebase/firestore'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDsvmNlI9E2Q2Nd6AKyt9R9Qu4Br5rcnvE",
-    authDomain: "nextjs-todo-4714d.firebaseapp.com",
-    projectId: "nextjs-todo-4714d",
-    storageBucket: "nextjs-todo-4714d.appspot.com",
-    messagingSenderId: "788797435161",
-    appId: "1:788797435161:web:ebd7529d30e4306ecbe988"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
